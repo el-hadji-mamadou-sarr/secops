613856e337b84f29427cf40f5bd2a8caef02fbec
el hadji <elhadji.sarr@outlook.com>
2025-07-11 15:35:25 +0200

    feat: optimized fastapi-server image using python3.9 slim image and change server port to 5006

--- fastapi-server/src/app.py
+++ fastapi-server/src/app.py
@@ -13,9 +13,9 @@ origins = [
 ]
 
 if (settings.debugging):
-    app = FastAPI(debug=True, reload=True, port=5000)
+    app = FastAPI(debug=True, reload=True, port=5006)
 else:
-    app = FastAPI(port=5000)
+    app = FastAPI(port=5006)
 
 
 @app.get("/healthcheck", include_in_schema=False)
